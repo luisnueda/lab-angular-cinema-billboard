b0c0ade07ce2d50dd8ec2feef90b717f6fedfaf1
luisnueda <nueda.luis@gmail.com>
2018-05-02 18:21:04 +0200

    Iteration 1 done

---
+++ starter-code/src/app/services/movies.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Movie } from '../movie.interface';
+import { moviesSamples } from '../../sample-movies';
+
+@Injectable()
+export class Movies {
+
+  movies: Array<Movie> = moviesSamples;
+
+  constructor() { }
+
+  getMovies() {
+    return this.movies;
+  }
+  getMovie(id) {
+    return this.movies.filter(  e => e.id === id);
+  }
+
+}
